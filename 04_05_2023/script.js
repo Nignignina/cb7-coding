bb6af5b1ff16566ad7ba41d4f7014bfae3ede936
Nignignina <131855794+Nignignina@users.noreply.github.com>
2023-05-04 16:14:32 +0200

    Add files via upload

---
+++ 04_05_2023/script.js
@@ -0,0 +1,21 @@
+
+
+
+let array = ["cane", "cipolla","gatto", "fanta"];
+
+let newArray= [];
+
+for (let i = array.length - 1; i >= 0; i--) {
+    let num = array[i];
+    newArray.push(num);
+} 
+
+console.log(newArray)
+
+
+// let myArray1 = [69, 87, 91, 469752, 121212121];
+// let minValue = Math.min(...myArray1)
+// let maxValue = Math.max(...myArray1)
+
+// console.log(minValue);
+// console.log(maxValue);
