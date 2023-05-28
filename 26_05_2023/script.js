8cfd4f6056f04345cb378d06d279a87f449103ae
Nignignina <131855794+Nignignina@users.noreply.github.com>
2023-05-28 23:37:14 +0200

    Add files via upload

--- 26_05_2023/script.js
+++ 26_05_2023/script.js
@@ -3,21 +3,37 @@ const qS = (element) => document.querySelector(element);
 // crea  li
 const noteGenerator = (pippo) => {
   const note = cE("li");
-
+ const iconCheck= cE("i")
+  const icontrash = cE("i");
+  icontrash.className = "fa fa-trash";
   note.textContent = pippo;
   const removeButton = cE("button");
-  removeButton.addEventListener("click", () => {
-    todoList = todoList.filter((todo) => todo.id !== pippo.id);
-    // mi sono bloccata a rimuovere l'elemento
-    
-    
-  });
+  note.className = "note";
+  const checkButton = cE("button")
+  checkButton.setAttribute("type", "button")
+  checkButton.className="check";
+  iconCheck.className ="fa-thin fa-circle-check";
   
-  removeButton.setAttribute("name", "remove");
-  removeButton.textContent = "rimuovi";
-  removeButton.className = "removeButton";
-  note.appendChild(removeButton);
 
+
+  removeButton.addEventListener("click", (e) => {
+    todoList = todoList.filter((todo) => todo.id !== pippo.id);
+    // console.log(todoList.indexOf(pippo));
+    // todoList.splice(index, 1)
+    // note.remove()
+    // console.log(index)
+
+    // console.log();
+    let index = todoList.findIndex((todo) => pippo === todo.id);
+    todoList.splice(index);
+    console.log(index);
+    note.remove();
+  });
+  removeButton.setAttribute("name", "remove");
+  removeButton.textContent = "";
+  removeButton.className = "removeButton";
+  removeButton.appendChild(icontrash);
+  note.append(removeButton, checkButton);
   return note;
 
   //   al click devo far si che e.target[0] sia aggiunto a li e storato in to dolist
@@ -32,7 +48,7 @@ const HandleSubmit = (e) => {
 
 const form = qS(".wrapperForm");
 const listItems = qS(".list");
-let todoList = [];
+let todoList =JSON.parse(localStorage.getItem("todoList")) || [];
 const textInput = qS(".inputText");
 
 const Render = () => {
@@ -41,6 +57,7 @@ const Render = () => {
   todoList.forEach((todo) => {
     listItems.appendChild(noteGenerator(todo.content));
   });
+  localStorage.setItem("todoList", JSON.stringify(todoList));
 };
 
 // quindi per ogni nota/oggetti di todolist devo creare un li da aggrappare a listItems(ul)
