de3bd4daf1c166ac7c4ba01cf699872fa5b62b5f
Nignignina <131855794+Nignignina@users.noreply.github.com>
2023-05-29 18:02:34 +0200

    Add files via upload

---
+++ 29_05_2023/script.js
@@ -0,0 +1,14 @@
+// const qS = (element) =>document.querySelector(element);
+
+// const navBar = qS(navBarItems)
+
+// navBar.array.forEach((element) => {
+//     element.addEventListener("click" ,() =>
+//     {
+//         navBar.forEach((item) => {
+//             item.classList.remove("active")
+//         })
+//     })
+//     element.classList.add("active")
+    
+// });
