26e939360e5258e7ba81955b41e684b01950c60b
Nignignina <131855794+Nignignina@users.noreply.github.com>
2023-05-30 20:21:36 +0200

    Add files via upload

--- 30_05_2023 - Copia/fn.js
+++ 30_05_2023 - Copia/fn.js
@@ -13,7 +13,9 @@ export const tweetGen = (tweetData) => {
   const nicknameProfile = cE("span");
   const textPost = cE("p");
   const reactionPost = cE("div");
-  const iconPost = cE("div");
+  const iconPost = cE("svg");
+  
+  
   const numberReaction = cE("span");
   const imagePlaceholder =
     "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg";
@@ -23,15 +25,17 @@ export const tweetGen = (tweetData) => {
   imgPost.src = tweetData.user?.image || imagePlaceholder;
   wrappertextPost.className = "wrapperText";
   nameProfile.textContent = tweetData.user?.firstName;
-  nicknameProfile.textContent = tweetData.user?.username;
-  textPost.textContent = tweetData.body;
+  nicknameProfile.textContent ="@"+ tweetData.user?.username;
+  textPost.textContent =tweetData.body;
   numberReaction.textContent = tweetData.reactions;
-
+  profileText.className ="userData"
+  nameProfile.className="nameProfile"
+  
   wrapperPost.append(profilePost, wrappertextPost);
   profilePost.append(imgPost);
-  wrappertextPost.append(profileText);
+  wrappertextPost.append(profileText, textPost, reactionPost);
 
-  profileText.append(nameProfile, nicknameProfile, iconPost);
+  profileText.append(nameProfile, nicknameProfile);
   reactionPost.append(iconPost, numberReaction);
 
   return wrapperPost;
